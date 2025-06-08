3131627f8badb0ef309404a7cc9271c79d4cefcf
Om2005Prakash <147433469+Om2005Prakash@users.noreply.github.com>
2025-06-08 12:09:28 +0000

    Deploying to gh-pages from @ Om2005Prakash/Om2005Prakash.github.io@6bef84a17c5e9dbebd022ff9eb87fb271797173f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/about/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-distill-style-blog-post",
         
           title: "a distill-style blog post",
